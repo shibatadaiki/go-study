95f1ae2d59a00aff09fc701e0ab1582b2a20918e
shibatadaiki <shibatadaiki92@gmail.com>
2020-10-01 05:06:50 +0900

    study net/http

--- server.go
+++ server.go
@@ -6,6 +6,8 @@ import (
   "encoding/json"
   "log"
   "fmt"
+  "time"
+  "math/rand"
 )
 
 // type ReturnJson struct {
@@ -17,30 +19,60 @@ import (
 // }
 
 type ReturnJson struct {
-   Status int    `json:"sample1"`
-   Result string `json:"sample2"`
-   }
+  Status int  `json:"sample1"`
+  Res1 string `json:"msg"`
+  Res2 string `json:"price"`
+  Body1 string `json:"bodymsg"`
+  Body2 string `json:"bodyprice"`
+}
 
-func jsonHandler(writer http.ResponseWriter, r *http.Request) {
-  returnJson := ReturnJson{http.StatusOK, "ok"}
+func jsonHandler(writer http.ResponseWriter, req *http.Request) {
+  defer req.Body.Close()
+
+  // POSTリクエスト（HTTPBodyにパラメーターを設定）の場合
+//   var returnJson ReturnJson
+//   err1 := json.NewDecoder(req.Body).Decode(&returnJson)
+//   if err1 != nil {
+//     log.Fatal(err1)
+//   }
+  // POSTリクエスト（HTTPBodyにパラメーターを設定）の場合
+
+  // GETリクエスト（クエリパラメーターを設定）の場合
+  empty := ""
+  returnJson := ReturnJson{http.StatusOK, req.FormValue("msg"), req.FormValue("price"), empty, empty}
+  // GETリクエスト（クエリパラメーターを設定）の場合
+
 
   res, err1 := json.Marshal(returnJson)
 
-  if err1 != nil {
-    http.Error(writer, err1.Error(), http.StatusInternalServerError)
+  // ステータスコード表 https://golang.org/src/net/http/status.go?h=Request
+  // ランダムに429と500を返すサンプルAPI
+  err_msg := "error!!!"
+  rand.Seed(time.Now().UnixNano())
+  ran := rand.Intn(100)
+  if ran % 20 == 0 {
+    http.Error(writer, err_msg, http.StatusTooManyRequests)
     return
-  }
-
-  dump, err2 := httputil.DumpRequest(r, true)
-  fmt.Println(string(dump))
-
-  if err2 != nil {
-    http.Error(writer, err2.Error(), http.StatusInternalServerError)
+  } else if ran % 15 == 0 {
+    http.Error(writer, err_msg, http.StatusInternalServerError)
     return
-  }
+  } else {
+    if err1 != nil {
+      http.Error(writer, err1.Error(), http.StatusInternalServerError)
+      return
+    }
 
-  writer.Header().Set("Content-Type", "application/json")
-  writer.Write(res)
+    dump, err2 := httputil.DumpRequest(req, true)
+    fmt.Println(string(dump))
+
+    if err2 != nil {
+      http.Error(writer, err2.Error(), http.StatusInternalServerError)
+      return
+    }
+
+    writer.Header().Set("Content-Type", "application/json")
+    writer.Write(res)
+  }
 }
 
 func httpSample() {
